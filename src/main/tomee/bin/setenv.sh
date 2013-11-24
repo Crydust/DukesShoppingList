426db6339297da15c11487f92e8a1ee7e0562d38
Kristof Neirynck <k.neirynck@gmail.com>
2013-11-24 13:44:00 +0100

    trying to get tomee-maven-plugin to work

---
+++ src/main/tomee/bin/setenv.sh
@@ -0,0 +1,3 @@
+CLASSPATH=%CATALINA_HOME%/conf/logback-config:%CATALINA_HOME%/lib/slf4j-api-1.7.5.jar:%CATALINA_HOME%/lib/jul-to-slf4j-1.7.5.jar:%CATALINA_HOME%/lib/jcl-over-slf4j-1.7.5.jar:%CATALINA_HOME%/lib/logback-core-1.0.13.jar:%CATALINA_HOME%/lib/logback-classic-1.0.13.jar
+
+CATALINA_OPTS=-Dfile.encoding=UTF-8\ -Dorg.apache.el.parser.COERCE_TO_ZERO=false
