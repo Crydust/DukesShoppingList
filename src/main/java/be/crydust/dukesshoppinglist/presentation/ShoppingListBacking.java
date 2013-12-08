7c57ec2f01aca6842a8173caabf71ee0798fe7f1
Kristof Neirynck <k.neirynck@gmail.com>
2013-12-08 22:25:58 +0100

    openjpa slf4j logging works

--- src/main/java/be/crydust/dukesshoppinglist/presentation/ShoppingListBacking.java
+++ src/main/java/be/crydust/dukesshoppinglist/presentation/ShoppingListBacking.java
@@ -1,30 +1,30 @@
 package be.crydust.dukesshoppinglist.presentation;
 
-//import be.crydust.dukesshoppinglist.business.shoppinglist.boundary.ShoppingListBoundary;
-//import be.crydust.dukesshoppinglist.business.shoppinglist.entity.ItemList;
-//import java.util.List;
-//import javax.enterprise.context.RequestScoped;
-//import javax.inject.Inject;
-//import javax.inject.Named;
-//
-///**
-// *
-// * @author kristof
-// */
-//@Named
-//@RequestScoped
+import be.crydust.dukesshoppinglist.business.shoppinglist.boundary.ShoppingListBoundary;
+import be.crydust.dukesshoppinglist.business.shoppinglist.entity.ItemList;
+import java.util.List;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+/**
+ *
+ * @author kristof
+ */
+@Named
+@RequestScoped
 public class ShoppingListBacking {
-//    
-//    @Inject
-//    ShoppingListBoundary boundary;
-//    
-//    List<ItemList> list = null;
-//    
-//    public List<ItemList> getLists(){
-//        if (list == null) {
-//            list = boundary.findAllShoppingLists();
-//        }
-//        return list;
-//    }
+    
+    @Inject
+    ShoppingListBoundary boundary;
+    
+    List<ItemList> list = null;
+    
+    public List<ItemList> getLists(){
+        if (list == null) {
+            list = boundary.findAllItemLists();
+        }
+        return list;
+    }
     
 }
--- src/main/java/be/crydust/dukesshoppinglist/presentation/StartupService.java
+++ src/main/java/be/crydust/dukesshoppinglist/presentation/StartupService.java
@@ -8,9 +8,7 @@ import be.crydust.dukesshoppinglist.business.shoppinglist.entity.ProductType;
 import be.crydust.dukesshoppinglist.business.shoppinglist.entity.Unit;
 import java.util.Arrays;
 import javax.annotation.PostConstruct;
-import javax.ejb.Startup;
 import javax.inject.Inject;
-import javax.inject.Singleton;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -18,36 +16,36 @@ import org.slf4j.LoggerFactory;
  *
  * @author kristof
  */
-//@Singleton
-//@Startup
+@javax.ejb.Singleton
+@javax.ejb.Startup
 public class StartupService {
 
-//    private static final Logger LOGGER = LoggerFactory.getLogger(StartupService.class);
-//
-//    @Inject
-//    ShoppingListBoundary boundary;
-//
-//    @PostConstruct
-//    public void onStartup() {
-//
-//        LOGGER.trace("onStartup");
-//
-//        boundary.deleteAllItemLists();
-//        ItemList list = new ItemList("list");
-//        ProductType type = new ProductType("type");
-//        Product product = new Product("product", type);
-//        Unit unit = new Unit("unit");
-//        Item item = new Item("5", unit, product);
-//        list.setItems(Arrays.asList(item));
-//        item.setItemList(list);
-//        boundary.saveItemList(list);
-//
-//        boundary.saveItemList(ItemList.createItemList("list2", Arrays.asList(
-//                new Item("1", new Unit("x"), new Product("Chicken", new ProductType("Meat"))),
-//                new Item("25", new Unit("Kg"), new Product("Potatoes", new ProductType("Vegetables"))),
-//                new Item("2", new Unit("bottles"), new Product("Wine", new ProductType("Beverages"))),
-//                new Item("1", new Unit("loaf"), new Product("Bread", new ProductType("Other")))
-//        )));
-//
-//    }
+    private static final Logger LOGGER = LoggerFactory.getLogger(StartupService.class);
+
+    @Inject
+    ShoppingListBoundary boundary;
+
+    @PostConstruct
+    public void onStartup() {
+
+        LOGGER.trace("StartupService.onStartup");
+
+        boundary.deleteAllItemLists();
+        ItemList list = new ItemList("list");
+        ProductType type = new ProductType("type");
+        Product product = new Product("product", type);
+        Unit unit = new Unit("unit");
+        Item item = new Item("5", unit, product);
+        list.setItems(Arrays.asList(item));
+        item.setItemList(list);
+        boundary.saveItemList(list);
+
+        boundary.saveItemList(ItemList.createItemList("list2", Arrays.asList(
+                new Item("1", new Unit("x"), new Product("Chicken", new ProductType("Meat"))),
+                new Item("25", new Unit("Kg"), new Product("Potatoes", new ProductType("Vegetables"))),
+                new Item("2", new Unit("bottles"), new Product("Wine", new ProductType("Beverages"))),
+                new Item("1", new Unit("loaf"), new Product("Bread", new ProductType("Other")))
+        )));
+
+    }
 }
